bdc4567fe637b5a3bb80f630be29f9c2feb67b7c
Marcos Daniel Petry <marcos.petry@corp.globo.com>
2013-06-18 02:39:53 -0300

    added modified date

--- examplesite/examplesite/tests/test_article_model.py
+++ examplesite/examplesite/tests/test_article_model.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+import time
 from django.core.urlresolvers import reverse
 from django.test import TestCase
 from press.models import Article
@@ -21,7 +23,10 @@ class ArticleModelTestCase(TestCase):
         self.assert_field_in('body', Article)
 
     def test_should_have_crated_date(self):
-        self.assert_field_in('crated_date', Article)
+        self.assert_field_in('created_date', Article)
+
+    def test_should_have_modified_date(self):
+        self.assert_field_in('modified_date', Article)
 
     def test_should_have_author(self):
         self.assert_field_in('author', Article)
@@ -44,3 +49,24 @@ class SavedArticle(TestCase):
         published_article = Article.objects.get(Article.Q_PUBLISHED, slug=self.article.slug)
         self.assertEqual(published_article.get_absolute_url(), article_url)
 
+    def test_created_and_modified_date_should_have_the_same_value_whe_it_publish_for_the_first_time(self):
+        time.sleep(2)
+        self.article.save()
+        self.article.publish()
+        published_article = Article.objects.get(Article.Q_PUBLISHED, slug=self.article.slug)
+        self.assertEqual(
+            datetime.strftime(published_article.created_date, "%H:%M:%S"),
+            datetime.strftime(published_article.modified_date, "%H:%M:%S")
+        )
+
+    def test_created_and_modified_date_should_be_differente_when_it_publish_for_second_time(self):
+        self.article.publish()
+        draft_article = Article.objects.get(Article.Q_DRAFT, slug=self.article.slug)
+        time.sleep(2)
+        draft_article.save()
+        draft_article.publish()
+        published_article_again = Article.objects.get(Article.Q_PUBLISHED, slug=self.article.slug)
+        self.assertGreater(
+            published_article_again.modified_date,
+            published_article_again.created_date
+        )
--- press/models.py
+++ press/models.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.models import User
 from django.db import models
+from django.utils import timezone
 from publish.models import Publishable
 from django.utils.translation import ugettext_lazy as _
 from django.core.urlresolvers import reverse as reverse_url
@@ -7,14 +8,15 @@ from django.core.urlresolvers import reverse as reverse_url
 
 class Article(Publishable):
     title = models.CharField(_('title'), max_length=200)
-    subtitle = models.CharField(_('subtitle'), max_length=500, blank=True)
     slug = models.CharField(_('slug'), max_length=100, db_index=True)
+    subtitle = models.CharField(_('subtitle'), max_length=500, blank=True)
     body = models.TextField(_('body'))
-    crated_date = models.DateTimeField(auto_now_add=True, editable=False)
+    created_date = models.DateTimeField(auto_now_add=True, editable=False, default=timezone.now())
+    modified_date = models.DateTimeField(auto_now=True, editable=False, default=timezone.now())
     author = models.ForeignKey(User)
 
     class Meta(Publishable.Meta):
-        ordering = ["crated_date"]
+        ordering = ["created_date"]
 
     def __unicode__(self):
         return self.title
