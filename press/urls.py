8718988adb38ec923cf6bb007f1b0326031814f9
Marcos Daniel Petry <marcos.petry@corp.globo.com>
2013-06-20 01:28:30 -0300

    move file

--- press/urls.py
+++ press/urls.py
@@ -4,6 +4,10 @@ from press.views import ArticleView
 
 urlpatterns = patterns(
     '',
-    url(r'^(?P<slug>[-_\w]+).html$', ArticleView.as_view(), kwargs={'type':'published'}, name="press-article-published"),
-    url(r'^draft/(?P<slug>[-_\w]+).html$', ArticleView.as_view(), kwargs={'type':'draft'}, name="press-article-draft"),
+    url(r'^(?P<slug>[-_\w]+).html$', ArticleView.as_view(),
+        kwargs={'type': 'published'},
+        name="press-article-published"),
+    url(r'^draft/(?P<slug>[-_\w]+).html$', ArticleView.as_view(),
+        kwargs={'type': 'draft'},
+        name="press-article-draft"),
 )
